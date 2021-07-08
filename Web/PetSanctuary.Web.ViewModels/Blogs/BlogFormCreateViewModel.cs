6133e78e4fc7c95ec5c545741f235ddbab2b5d35
Alexander Stoyanov <63510557+aleksanderstoyanov@users.noreply.github.com>
2021-07-09 00:22:26 +0300

    Adding Blog Controller alongside views and view models

---
+++ Web/PetSanctuary.Web.ViewModels/Blogs/BlogFormCreateViewModel.cs
@@ -0,0 +1,27 @@
+﻿using PetSanctuary.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+
+
+
+namespace PetSanctuary.Web.ViewModels.Blogs
+{
+    public class BlogFormCreateViewModel
+    {
+        [Required]
+        [MinLength(GlobalConstants.MinBlogTitleLength)]
+        [MaxLength(GlobalConstants.MaxBlogTitleLength)]
+        public string Title { get; set; }
+        [Required]
+        [Display(Name = "Image-Url")]
+        [Url]
+        public string Image { get; set; }
+        [Required]
+        [MaxLength(GlobalConstants.MaxBlogDescriptionLength)]
+        public string Description { get; set; }
+
+    }
+}
---
+++ Web/PetSanctuary.Web/Controllers/BlogsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetSanctuary.Web.ViewModels.Blogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetSanctuary.Web.Controllers
+{
+    public class BlogsController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+        public IActionResult Comments()
+        {
+            return this.View();
+        }
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+        [HttpPost]
+        public IActionResult Create(BlogFormCreateViewModel model)
+        {
+            return this.Redirect("/Blogs");
+        }
+        
+    }
+}
