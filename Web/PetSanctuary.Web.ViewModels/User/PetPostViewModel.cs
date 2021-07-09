b754be9b81ed83134f9a075726c2890a94b51a52
Alexander Stoyanov <63510557+aleksanderstoyanov@users.noreply.github.com>
2021-07-09 22:49:07 +0300

    Adding minor fixes

--- Web/PetSanctuary.Web.ViewModels/User/PetPostViewModel.cs
+++ Web/PetSanctuary.Web.ViewModels/User/PetPostViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PetSanctuary.Common;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
@@ -9,19 +10,28 @@ namespace PetSanctuary.Web.ViewModels.User
     {
         public string Id { get; set; }
         [Required]
+        [MinLength(GlobalConstants.MinPetNameLength)]
+        [MaxLength(GlobalConstants.MaxPetNameLength)]
         public string Name { get; set; }
         [Required]
         [Range(1, 18)]
         public int Age { get; set; }
         [Required]
+        
         public string Type { get; set; }
         [Required]
         public string Gender { get; set; }
         [Required]
+        [Display(Name = "Image-Url")]
+        [Url]
         public string Image { get; set; }
         [Required]
+        [MinLength(GlobalConstants.MinCityLength)]
+        [MaxLength(GlobalConstants.MaxCityLength)]
         public string City { get; set; }
         [Required]
+        [MinLength(GlobalConstants.MinAddressLength)]
+        [MaxLength(GlobalConstants.MaxAddressLength)]
         public string Address { get; set; }
         [Required]
         public string IsVaccinated { get; set; }
