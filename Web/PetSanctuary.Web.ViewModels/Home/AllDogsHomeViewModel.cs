a9b103457ffce2de30d54d1148364bb2cba89047
Alexander Stoyanov <63510557+aleksanderstoyanov@users.noreply.github.com>
2021-07-14 11:00:55 +0300

    Modifying view imports and implementing new visualisation in home controller

---
+++ Web/PetSanctuary.Web.ViewModels/Home/AllDogsHomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetSanctuary.Web.ViewModels.Home
+{
+    public class AllDogsHomeViewModel
+    {
+        public string Id { get; set; }
+
+        public string Image { get; set; }
+
+        public string Name { get; set; }
+    }
+}
--- Web/PetSanctuary.Web/Controllers/HomeController.cs
+++ Web/PetSanctuary.Web/Controllers/HomeController.cs
@@ -1,16 +1,31 @@
 ﻿namespace PetSanctuary.Web.Controllers
 {
     using System.Diagnostics;
-
     using PetSanctuary.Web.ViewModels;
-
     using Microsoft.AspNetCore.Mvc;
+    using PetSanctuary.Services.Data.Catalogs;
+    using System.Linq;
+    using PetSanctuary.Web.ViewModels.Home;
 
     public class HomeController : BaseController
     {
+        private readonly ICatalogService catalogService;
+
+        public HomeController(ICatalogService catalogService)
+        {
+            this.catalogService = catalogService;
+        }
+
         public IActionResult Index()
         {
-            return this.View();
+            var dogs = this.catalogService.GetAllDogs()
+                .Select(x => new AllDogsHomeViewModel
+                {
+                    Id = x.Id,
+                    Image = x.Image,
+                    Name = x.Name
+                }).ToList();
+            return this.View(dogs);
         }
 
         public IActionResult Privacy()
