b43a6ef63e3b93a2ecbec34f2b25d4cdfe677dfe
Alexander Stoyanov <63510557+aleksanderstoyanov@users.noreply.github.com>
2021-07-21 00:10:12 +0300

    Adding blog and comment service models

---
+++ Services/PetSanctuary.Services.Data/Blogs/BlogServiceModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetSanctuary.Services.Data.Blogs
+{
+    public class BlogServiceModel
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Image { get; set; }
+
+        public string Description { get; set; }
+
+        public string Author { get; set; }
+
+        public string CreatedOn { get; set; }
+    }
+}
---
+++ Services/PetSanctuary.Services.Data/Comments/CommentServiceModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetSanctuary.Services.Data.Comments
+{
+    public class CommentServiceModel
+    {
+        public int Id { get; set; }
+
+        public string Content { get; set; }
+
+        public string BlogId { get; set; }
+
+        public string PublishedOn { get; set; }
+
+        public string Publisher { get; set; }
+
+    }
+}
