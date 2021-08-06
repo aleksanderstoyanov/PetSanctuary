79a49936c411b3ab62430fabe7c5181e91d9d206
Alexander Stoyanov <63510557+aleksanderstoyanov@users.noreply.github.com>
2021-08-06 18:12:24 +0300

    Adding routing and controller tests for blogs

---
+++ PetSanctuary.Test/Controllers/BlogsControllerTest.cs
@@ -0,0 +1,65 @@
+﻿namespace PetSanctuary.Test.Controllers
+{
+    using PetSanctuary.Web.Controllers;
+    using MyTested.AspNetCore.Mvc;
+    using Xunit;
+    using PetSanctuary.Services.Data.Blogs;
+    using System.Collections.Generic;
+    using PetSanctuary.Data.Models;
+    using Microsoft.AspNetCore.Http;
+    using System.IO;
+    using PetSanctuary.Web.ViewModels.Blogs;
+    using System.Linq;
+
+    public class BlogsControllerTest
+    {
+        [Fact]
+        public void IndexShouldReturnProperView()
+            => MyController<BlogsController>
+            .Instance()
+            .Calling(c => c.Index())
+            .ShouldReturn()
+            .View(view => view
+                .WithModelOfType<List<BlogServiceModel>>());
+
+
+        [Fact]
+        public void GetCreateShouldReturnProperViewAndShouldBeWithAuthorizedUsers()
+            => MyController<BlogsController>
+            .Instance()
+            .Calling(c => c.Create())
+            .ShouldHave()
+            .ActionAttributes(attributes => attributes
+                 .RestrictingForAuthorizedRequests())
+            .AndAlso()
+            .ShouldReturn()
+            .View();
+
+        [Fact]
+        public void PostCreateShouldReturnProperViewAndShouldBeWithAuthorizedUsersAndWithValidModel()
+           => MyController<BlogsController>
+            .Instance(c => c
+               .WithUser())
+            .Calling(c => c.Create(new BlogFormCreateViewModel
+            {
+                Title = "TestTitle",
+                Description = "Test description blog",
+                Image = new FormFile(File.OpenRead(@"C:\Users\black\OneDrive\Desktop\PetSanctuary\Web\PetSanctuary.Web\wwwroot\img\Test.png"), 0, "Test.png".Length, null, "Test.png"),
+            }))
+            .ShouldHave()
+            .ActionAttributes(attributes=>attributes
+                    .RestrictingForAuthorizedRequests()
+                    .RestrictingForHttpMethod(HttpMethod.Post))
+            .AndAlso()
+            .ShouldHave()
+            .ValidModelState()
+              .Data(data => data.WithSet<Blog>(blogs => blogs.Any(blog => blog.Title == "TestTitle")))
+            .AndAlso()
+            .ShouldReturn()
+            .Redirect("/Blogs");
+            
+           
+
+
+    }
+}
---
+++ PetSanctuary.Test/Routing/BlogsControllerTest.cs
@@ -0,0 +1,34 @@
+﻿namespace PetSanctuary.Test.Routing
+{
+    using PetSanctuary.Web.Controllers;
+    using MyTested.AspNetCore.Mvc;
+    using Xunit;
+    using PetSanctuary.Web.ViewModels.Blogs;
+
+    public class BlogsControllerTest
+    {
+        [Fact]
+        public void IndexShouldBeMappedCorrectly()
+            => MyRouting
+            .Configuration()
+            .ShouldMap("/Blogs")
+            .To<BlogsController>(c => c.Index());
+
+        [Fact]
+        public void GetCreateShouldBeMappedCorrectly()
+             => MyRouting
+            .Configuration()
+            .ShouldMap("/Blogs/Create")
+            .To<BlogsController>(c => c.Create());
+
+        [Fact]
+        public void PostCreateShouldBeMappedCorrectlyAndWithValidModel()
+             => MyRouting
+            .Configuration()
+            .ShouldMap(request => request
+                   .WithPath("/Blogs/Create")
+                   .WithMethod(HttpMethod.Post))
+            .To<BlogsController>(c => c
+                 .Create(new BlogFormCreateViewModel()));
+    }
+}
