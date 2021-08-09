8c12415028efe711c24ab49a977d6aedce3efeeb
Alexander Stoyanov <63510557+aleksanderstoyanov@users.noreply.github.com>
2021-08-09 20:26:21 +0300

    Added routing test for clinics and vet controllers for administration area

---
+++ PetSanctuary.Test/Routing/Admin/ClinicsControllerTest.cs
@@ -0,0 +1,59 @@
+﻿namespace PetSanctuary.Test.Routing.Admin
+{
+    using PetSanctuary.Web.Areas.Administration.Controllers;
+    using MyTested.AspNetCore.Mvc;
+    using Xunit;
+    using PetSanctuary.Web.ViewModels.Administration.Clinics;
+
+    public class ClinicsControllerTest
+    {
+        [Fact]
+        public void GetCreateShouldBeMappedCorrectly()
+             => MyRouting
+            .Configuration()
+            .ShouldMap(request => request
+                .WithMethod(HttpMethod.Get)
+                .WithPath("/Administration/Clinics/Create"))
+            .To<ClinicsController>(c => c
+                .Create());
+
+        [Fact]
+        public void PostCreateShouldBeMappedCorrectly()
+            => MyRouting
+             .Configuration()
+             .ShouldMap(request => request
+                  .WithMethod(HttpMethod.Post)
+                  .WithPath("/Administration/Clinics/Create"))
+            .To<ClinicsController>(c => c
+                 .Create(new ClinicInputModel()));
+
+        [Fact]
+        public void GetEditShouldBeMappedCorrectly()
+             => MyRouting
+            .Configuration()
+            .ShouldMap(request => request
+                  .WithMethod(HttpMethod.Get)
+                  .WithPath("/Administration/Clinics/Edit/1"))
+            .To<ClinicsController>(c => c
+               .Edit(1));
+
+        [Fact]
+        public void PostEditShouldBeMappedCorrectly()
+             => MyRouting
+            .Configuration()
+            .ShouldMap(request => request
+                  .WithMethod(HttpMethod.Post)
+                  .WithAntiForgeryToken()
+                  .WithPath("/Administration/Clinics/Edit/1"))
+            .To<ClinicsController>(c => c
+                 .Edit(1, new ClinicInputModel()));
+
+        [Fact]
+        public void DeleteShouldBeMappedCorrectly()
+              => MyRouting
+            .Configuration()
+              .ShouldMap("/Administration/Clinics/Delete/1")
+            .To<ClinicsController>(c => c
+                .Delete(1));
+    }
+}
---
+++ PetSanctuary.Test/Routing/Admin/VetsControllerTest.cs
@@ -0,0 +1,61 @@
+﻿namespace PetSanctuary.Test.Routing.Admin
+{
+    using PetSanctuary.Web.Areas.Administration.Controllers;
+    using MyTested.AspNetCore.Mvc;
+    using Xunit;
+    using PetSanctuary.Web.ViewModels.Administration.Vets;
+
+    public class VetsControllerTest
+    {
+        [Fact]
+        public void GetCreateShouldBeMappedCorrectly()
+             => MyRouting
+              .Configuration()
+               .ShouldMap(request => request
+                  .WithPath("/Administration/Vets/Create")
+                  .WithMethod(HttpMethod.Get))
+            .To<VetsController>(c => c
+                .Create());
+
+        [Fact]
+        public void PostCreateShouldBeMappedCorrectly()
+              => MyRouting
+                .Configuration()
+                 .ShouldMap(request => request
+                   .WithPath("/Administration/Vets/Create/TestId")
+                   .WithMethod(HttpMethod.Post))
+            .To<VetsController>(c => c
+                .Create(new VetInputModel()));
+
+        [Fact]
+        public void GetEditShouldBeMappedCorrectly()
+              => MyRouting
+               .Configuration()
+                 .ShouldMap(request => request
+                     .WithPath("/Administration/Vets/Edit/TestId"))
+            .To<VetsController>(c => c
+              .Edit("TestId"));
+
+        [Fact]
+        public void PostEditShouldBeMappedCorrectly()
+             => MyRouting
+              .Configuration()
+                .ShouldMap(request => request
+                    .WithPath("/Administration/Vets/Edit/TestId")
+                    .WithMethod(HttpMethod.Post)
+                    .WithQuery("clinicId", "1"))
+            .To<VetsController>(c => c
+                .Edit(1, "TestId", new VetInputModel()));
+
+        [Fact]
+        public void DeleteShouldBeMappedCorrectly()
+             => MyRouting
+             .Configuration()
+               .ShouldMap(request => request
+                  .WithPath("/Administration/Vets/Delete/TestId")
+                  .WithQuery("clinicId", "1"))
+            .To<VetsController>(c => c
+               .Delete(1, "TestId"));
+
+    }
+}
