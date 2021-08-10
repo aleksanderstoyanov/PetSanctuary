33c7a23a858395084b66952bc190e36f02aa73b5
Alexander Stoyanov <63510557+aleksanderstoyanov@users.noreply.github.com>
2021-08-10 23:30:41 +0300

    Added caching for vets

--- Services/PetSanctuary.Services.Data/Vets/VetService.cs
+++ Services/PetSanctuary.Services.Data/Vets/VetService.cs
@@ -1,9 +1,10 @@
 ﻿namespace PetSanctuary.Services.Data.Vets
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
-
+    using Microsoft.Extensions.Caching.Memory;
     using PetSanctuary.Data.Common.Repositories;
     using PetSanctuary.Data.Models;
     using PetSanctuary.Services.Mapping;
@@ -11,10 +12,12 @@
     public class VetService : IVetService
     {
         private readonly IDeletableEntityRepository<Vet> vetsRepository;
+        private readonly IMemoryCache cache;
 
-        public VetService(IDeletableEntityRepository<Vet> vetsRepository)
+        public VetService(IDeletableEntityRepository<Vet> vetsRepository, IMemoryCache cache)
         {
             this.vetsRepository = vetsRepository;
+            this.cache = cache;
         }
 
         public VetServiceModel GetVetById(string id)
@@ -28,11 +31,20 @@
 
         public IEnumerable<VetServiceModel> GetVetsById(int clinicId)
         {
-            return this.vetsRepository
-              .AllAsNoTracking()
-              .Where(vet => vet.ClinicId == clinicId)
-              .To<VetServiceModel>()
-              .ToList();
+            string memoryKey = $"VetsKey{clinicId}";
+            var vets = this.cache.Get<List<VetServiceModel>>(memoryKey);
+            if (vets == null)
+            {
+                vets = this.vetsRepository
+                 .AllAsNoTracking()
+                 .Where(vet => vet.ClinicId == clinicId)
+                 .To<VetServiceModel>()
+                 .ToList();
+                this.cache.Set(memoryKey, vets, new MemoryCacheEntryOptions()
+                      .SetAbsoluteExpiration(TimeSpan.FromMinutes(10)));
+            }
+
+            return vets;
         }
 
         public async Task UpdateDislikesAsync(string vetId)
