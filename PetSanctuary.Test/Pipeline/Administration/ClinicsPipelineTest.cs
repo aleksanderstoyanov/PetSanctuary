1efaa48d822080ca188885a1324d947ce2223c7b
Alexander Stoyanov <63510557+aleksanderstoyanov@users.noreply.github.com>
2021-08-17 17:19:36 +0300

    Added administration pipeline tests for clinics and vets

---
+++ PetSanctuary.Test/Pipeline/Administration/ClinicsPipelineTest.cs
@@ -0,0 +1,135 @@
+﻿namespace PetSanctuary.Test.Pipeline.Administration
+{
+    using PetSanctuary.Web.Areas.Administration.Controllers;
+    using MyTested.AspNetCore.Mvc;
+    using Xunit;
+    using PetSanctuary.Common;
+    using PetSanctuary.Web.ViewModels.Administration.Clinics;
+    using PetSanctuary.Data.Models;
+    using System.Linq;
+    using PetSanctuary.Test.Data;
+
+    public class ClinicsPipelineTest
+    {
+        [Fact]
+        public void GetCreateShouldReturnDefaultView()
+            => MyPipeline
+            .Configuration()
+            .ShouldMap(request => request
+                .WithLocation("/Administration/Clinics/Create")
+                .WithUser(new[] { GlobalConstants.AdministratorRoleName }))
+            .To<ClinicsController>(c => c
+                .Create())
+            .Which()
+            .ShouldReturn()
+            .View();
+
+        [Theory]
+        [InlineData("TestName", "TestAddress", "TestCity", "http://mandoubapp.com/public/uploads/doctors/r8VRdtgphyjjSm32Azly8uTxBa5cHJ7EwdAlSZZi.jpg")]
+        public void PostCreateShouldReturnDefaultRedirectToActionAndShouldBeWithValidModelState(string name, string address, string city, string image)
+            => MyPipeline
+            .Configuration()
+            .ShouldMap(request => request
+                .WithMethod(HttpMethod.Post)
+                .WithFormFields(new
+                {
+                    Name = name,
+                    Address = address,
+                    City = city,
+                    Image = image
+                })
+                .WithUser(new[] { GlobalConstants.AdministratorRoleName })
+                .WithAntiForgeryToken()
+                .WithLocation("/Administration/Clinics/Create"))
+            .To<ClinicsController>(c => c.Create(new ClinicInputModel
+            {
+                Name = name,
+                Address = address,
+                City = city,
+                Image = image
+            }))
+            .Which()
+            .ShouldHave()
+            .ActionAttributes(attributes => attributes
+                .RestrictingForHttpMethod(HttpMethod.Post))
+            .AndAlso()
+            .ShouldHave()
+            .ValidModelState()
+            .Data(data => data
+                .WithSet<Clinic>(data => data.Any()))
+            .AndAlso()
+            .ShouldReturn()
+            .Redirect("/Clinics/Index");
+
+        [Fact]
+        public void GetEditShouldReturnDefaultView()
+            => MyPipeline
+            .Configuration()
+            .ShouldMap(request => request
+                 .WithLocation("/Administration/Clinics/Edit/1")
+                 .WithUser(new[] { GlobalConstants.AdministratorRoleName }))
+            .To<ClinicsController>(c => c.Edit(1))
+            .Which(controller => controller
+               .WithData(ClinicsTestData.GetClinics(1, "Sofia")))
+            .ShouldReturn()
+            .View(result => result
+               .WithModelOfType<ClinicInputModel>());
+
+        [Theory]
+        [InlineData("EditedName", "TestAddress", "TestCity", "http://mandoubapp.com/public/uploads/doctors/r8VRdtgphyjjSm32Azly8uTxBa5cHJ7EwdAlSZZi.jpg")]
+        public void PostEditShouldReturnDefaultRedirectToActionAndShouldBeWithValidModelState(string name, string address, string city, string image)
+            => MyPipeline
+            .Configuration()
+            .ShouldMap(request => request
+                 .WithMethod(HttpMethod.Post)
+                 .WithLocation("/Administration/Clinics/Edit/1")
+                 .WithFormFields(new
+                 {
+                     Name = name,
+                     Address = address,
+                     City = city,
+                     Image = image
+                 })
+                 .WithUser(new[] { GlobalConstants.AdministratorRoleName })
+                 .WithAntiForgeryToken())
+            .To<ClinicsController>(c => c.Edit(1, new ClinicInputModel
+            {
+                Name = name,
+                Address = address,
+                City = city,
+                Image = image
+            }))
+            .Which(controller => controller
+                .WithData(ClinicsTestData.GetClinics(1, "Sofia")))
+            .ShouldHave()
+            .ActionAttributes(attributes => attributes
+                .RestrictingForHttpMethod(HttpMethod.Post))
+            .AndAlso()
+            .ShouldHave()
+            .ValidModelState()
+            .Data(data => data
+                .WithSet<Clinic>(data => data
+                   .Any(data => data.Name == name)))
+            .AndAlso()
+            .ShouldReturn()
+            .Redirect("/Clinics/Index");
+
+        [Fact]
+        public void GetDeleteShouldReturnDefaultRedirectToAction()
+            => MyPipeline
+            .Configuration()
+            .ShouldMap(request => request
+               .WithLocation("Administration/Clinics/Delete/1")
+               .WithUser(new[] { GlobalConstants.AdministratorRoleName }))
+            .To<ClinicsController>(c => c.Delete(1))
+            .Which(controller => controller
+               .WithData(ClinicsTestData.GetClinics(1, "Sofia")))
+            .ShouldHave()
+            .Data(data => data
+              .WithSet<Clinic>(data => !data.Any()))
+            .AndAlso()
+            .ShouldReturn()
+            .Redirect("/Clinics/Index");
+
+    }
+}
---
+++ PetSanctuary.Test/Pipeline/Administration/VetsPipelineTest.cs
@@ -0,0 +1,144 @@
+﻿namespace PetSanctuary.Test.Pipeline.Administration
+{
+    using PetSanctuary.Web.Areas.Administration.Controllers;
+    using MyTested.AspNetCore.Mvc;
+    using Xunit;
+    using PetSanctuary.Common;
+    using PetSanctuary.Web.ViewModels.Administration.Vets;
+    using PetSanctuary.Data.Models;
+    using System.Linq;
+    using PetSanctuary.Test.Data;
+
+    public class VetsPipelineTest
+    {
+        [Fact]
+        public void GetCreateShouldReturnDefaultView()
+            => MyPipeline
+            .Configuration()
+            .ShouldMap(request => request
+              .WithLocation("/Administration/Vets/Create")
+              .WithUser(new[] { GlobalConstants.AdministratorRoleName }))
+            .To<VetsController>(c => c.Create())
+            .Which()
+            .ShouldReturn()
+            .View();
+
+        [Theory]
+        [InlineData("TestFirstName", "TestSurname", "Test1", "Veterinarian", "Test Description...")]
+        public void PostCreateShouldReturnDefaultRedirectToActionWithValidModelState(string firstName, string surname, string clinic, string qualification, string description)
+            => MyPipeline
+            .Configuration()
+            .ShouldMap(request => request
+                .WithMethod(HttpMethod.Post)
+                .WithLocation("/Administration/Vets/Create/TestId")
+                .WithFormFields(new
+                {
+                    FirstName = firstName,
+                    Surname = surname,
+                    Clinic = clinic,
+                    Qualification = qualification,
+                    Description = description
+                })
+                .WithUser(new[] { GlobalConstants.AdministratorRoleName })
+                .WithAntiForgeryToken())
+               .To<VetsController>(c => c.Create(new VetInputModel
+               {
+                   FirstName = firstName,
+                   Surname = surname,
+                   Clinic = clinic,
+                   Qualification = qualification,
+                   Description = description
+               }))
+            .Which(controller => controller
+              .WithData(ClinicsTestData.GetClinics(1, "Sofia")))
+            .ShouldHave()
+            .ActionAttributes(attributes => attributes
+               .RestrictingForHttpMethod(HttpMethod.Post))
+            .AndAlso()
+            .ShouldHave()
+            .ValidModelState()
+            .Data(data => data
+              .WithSet<Vet>(data => data.Any()))
+            .AndAlso()
+            .ShouldReturn()
+            .Redirect("/Clinics/Index");
+
+
+        [Fact]
+        public void GetEditShouldReturnDefaultView()
+            => MyPipeline
+            .Configuration()
+            .ShouldMap(request => request
+               .WithLocation("/Administration/Vets/Edit/TestId1")
+               .WithUser(new[] { GlobalConstants.AdministratorRoleName }))
+            .To<VetsController>(c => c.Edit("TestId1"))
+            .Which(controller => controller
+               .WithData(VetsTestData.GetVets(1)))
+            .ShouldReturn()
+            .View(result => result
+               .WithModelOfType<VetInputModel>());
+
+        [Theory]
+        [InlineData("EditedFirstName", "TestSurname", "Test", "Veterinarian", "Test Description...")]
+        public void PostEditShouldReturnDefaultRedirectToActionWithValidModelState(string firstName, string surname, string clinic, string qualification, string description)
+            => MyPipeline
+            .Configuration()
+            .ShouldMap(request => request
+                .WithMethod(HttpMethod.Post)
+                .WithQuery("clinicId","1")
+                .WithLocation("/Administration/Vets/Edit/TestId1")
+                .WithFormFields(new
+                {
+                    FirstName = firstName,
+                    Surname = surname,
+                    Clinic = clinic,
+                    Qualification = qualification,
+                    Description = description
+                })
+                .WithUser(new[] { GlobalConstants.AdministratorRoleName })
+                .WithAntiForgeryToken())
+            .To<VetsController>(c => c.Edit(1, "TestId1", new VetInputModel
+            {
+                FirstName = firstName,
+                Surname = surname,
+                Clinic = clinic,
+                Qualification = qualification,
+                Description = description
+            }))
+            .Which(controller => controller
+               .WithData(VetsTestData.GetVets(1)))
+            .ShouldHave()
+            .ActionAttributes(attributes => attributes
+                .RestrictingForHttpMethod(HttpMethod.Post))
+            .AndAlso()
+            .ShouldHave()
+            .ValidModelState()
+            .Data(data => data
+              .WithSet<Vet>(data => data.Any(model => model.FirstName == firstName)))
+            .AndAlso()
+            .ShouldReturn()
+            .Redirect("/Vets/Index/1");
+
+
+        [Fact]
+        public void GetDeleteShouldReturnDefaultRedirectToAction()
+            => MyPipeline
+            .Configuration()
+            .ShouldMap(request => request
+                .WithLocation("/Administration/Vets/Delete/TestId1")
+                .WithQuery("clinicId","1")
+                .WithUser(new[] { GlobalConstants.AdministratorRoleName }))
+            .To<VetsController>(c => c.Delete(1, "TestId1"))
+            .Which(controller => controller
+               .WithData(VetsTestData.GetVets(1)))
+            .ShouldHave()
+            .Data(data => data
+               .WithSet<Vet>(data => !data.Any()))
+            .AndAlso()
+            .ShouldReturn()
+            .Redirect("/Vets/Index/1");
+
+
+
+    }
+}
