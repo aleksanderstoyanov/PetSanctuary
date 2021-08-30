4dfb6b1107b0aea236700400fd477f5c7155c013
Alexander Stoyanov <63510557+aleksanderstoyanov@users.noreply.github.com>
2021-08-30 15:59:08 +0300

    Adding statistics api controller with ajax

---
+++ Web/PetSanctuary.Web.ViewModels/Api/Statistics/StatisticsResponseModel.cs
@@ -0,0 +1,15 @@
+﻿namespace PetSanctuary.Web.ViewModels.Api.Statistics
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class StatisticsResponseModel
+    {
+        public int TotalDogs { get; set; }
+
+        public int TotalCats { get; set; }
+
+        public int TotalOthers { get; set; }
+    }
+}
---
+++ Web/PetSanctuary.Web/Controllers/Api/StatisticsController.cs
@@ -0,0 +1,37 @@
+﻿namespace PetSanctuary.Web.Controllers.Api
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+    using PetSanctuary.Services.Data.Counts;
+    using PetSanctuary.Web.ViewModels.Api.Statistics;
+
+    [ApiController]
+    [Route("api/statistics")]
+    public class StatisticsController : ControllerBase
+    {
+        private readonly ICountService countService;
+
+        public StatisticsController(ICountService countService)
+        {
+            this.countService = countService;
+        }
+
+        [HttpGet]
+        public ActionResult<StatisticsResponseModel> GetStatistics()
+        {
+            var totalDogs = this.countService.GetTotalPetsByType("Dog");
+            var totalCats = this.countService.GetTotalPetsByType("Cat");
+            var totalOthers = this.countService.GetTotalPetsByType("Other");
+            return new StatisticsResponseModel()
+            {
+                TotalDogs = totalDogs,
+                TotalCats = totalCats,
+                TotalOthers = totalOthers,
+            };
+        }
+    }
+}
